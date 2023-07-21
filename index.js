3522a93d60d112fbed193e623cdc8240faabb1b4
Lactor <lactorjm4@gmail.com>
2023-07-21 16:40:14 +0300

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+
+  function distanceFromHqInBlocks(someValue) {
+    if (someValue>=42){
+    return (someValue-42);
+    } else{
+        return (42-someValue);
+    }
+  }  
+distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue)*264;
+}
+distanceFromHqInFeet(43);
+
+function distanceTravelledInFeet(start, destination){
+    if (destination>43){
+        return (destination-start)*264;
+    } else{
+        return (start-destination)*264;
+    }
+}
+distanceTravelledInFeet(43,50);
+
+function calculatesFarePrice(start, destination){
+    const distancePrice = distanceTravelledInFeet(start, destination);
+
+    if (distancePrice <= 400) {
+        return 0; 
+    } else if (distancePrice <= 2000) {
+        return (distancePrice - 400) * 0.02; 
+    } else if (distancePrice <= 2500) {
+        return 25; 
+    } else {
+        return "cannot travel that far"; 
+    }
+}
+calculatesFarePrice(43,50);
